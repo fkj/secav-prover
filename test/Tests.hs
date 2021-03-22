ab324465d8fd7e1b0239e93d45b3a59530e1beb2
Frederik Jacobsen <fkjacobsen@gmail.com>
2021-03-22 13:16:51 +0100

    Add a number of propositional tests.

--- test/Tests.hs
+++ test/Tests.hs
@@ -2,5 +2,12 @@ module Tests where
 
 testcases :: [(String, String)]
 testcases =
-  [ ("imp", "Imp a a")
+  [ ("true", "Imp a a")
+  , ("false", "Neg (Neg (Imp a a))")
+  , ("negations", "Imp (Neg p) (Neg (Neg (Neg p)))")
+  , ("excludedMiddle", "Dis a (Neg a)")
+  , ("excludedMiddle2", "Dis (Neg a) a")
+  , ("impNeg", "Dis p (Imp p q)")
+  , ("extraCon", "Imp (Con p q) (Imp r (Con p r))")
+  , ("bigCon", "Con (Con (Imp a a) (Imp b b)) (Con (Imp c c) (Imp d d))")
   ]
