c0044263cca7669a41e94c5b26539bec54d6764f
Frederik Jacobsen <fkjacobsen@gmail.com>
2021-03-22 10:31:22 +0100

    Add better handling of command line arguments and option to output Isabelle.

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -5,19 +5,48 @@ import SeCaVGenerator
 import ProofExtractor
 import SeCaV_Enum
 
-import System.Environment
+import Options.Applicative
+import qualified ProofParser
+import IsabelleGenerator (genProgram)
+
+data Arguments = Arguments
+  { formula :: String
+  , isabelle :: Bool
+  }
+
+arguments :: Parser Arguments
+arguments = Arguments
+            <$> argument str
+            (  metavar "FORMULA"
+            <> help "Formula to attempt to prove" )
+            <*> switch
+            ( long "isabelle"
+            <> short 'i'
+            <> help "Whether to output an Isabelle proof" )
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [] -> print "Too few arguments"
-    [x] -> case parser x of
-             Left error -> print error
-             Right sequent ->
-               let result = genInit sequent in
-               let formulas = fst result in
-               let names = snd result in
-               let proof = secavTree formulas in
-               putStrLn $ extract names proof
-    _ -> print "Too many arguments"
+main = run =<< execParser opts
+  where
+    opts = info (arguments <**> helper)
+      ( fullDesc
+      <> progDesc "Attempt to prove a first-order formula"
+      <> header "secav-prover - a prover for SeCaV" )
+
+run :: Arguments -> IO ()
+run (Arguments formula isabelle) =
+  case parser formula of
+    Left error -> print error
+    Right sequent ->
+      let (formulas, names) = genInit sequent in
+        let proof = secavTree formulas in
+          let short = extract names proof in
+            if isabelle
+            then
+              let parse = ProofParser.parser short in
+                case parse of
+                  Left e -> print $ show e
+                  Right ast ->
+                    let isabelleProof = genProgram ast in
+                      putStrLn isabelleProof
+            else
+              putStrLn short
