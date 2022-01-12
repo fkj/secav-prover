3e1562c1a8f4aea37407fbd5d0d88e5dd8be28dc
Frederik Krogsdal Jacobsen <fkjac@dtu.dk>
2022-01-12 16:49:37 +0100

    Add a soundness test.

--- test/soundness/Tests.hs
+++ test/soundness/Tests.hs
@@ -26,5 +26,6 @@ testcases =
     ("ex7.4c", "Imp (Dis (Uni A[0]) (Exi B[0])) (Uni (Dis A[0] B[0]))"),
     ("ex7.4d", "Imp (Imp (Exi A[0]) (Exi B[0])) (Uni (Imp A[0] B[0]))"),
     ("ex7.9", "Imp (Imp (Uni p[0]) (Uni q[0])) (Uni (Imp p[0] q[0]))"),
-    ("ex9.4", "Imp (Uni (Exi A[1,0])) (Uni A[0, f[0]])")
+    ("ex9.4", "Imp (Uni (Exi A[1,0])) (Uni A[0, f[0]])"),
+    ("multiDelta", "Dis (Uni P[0]) (Uni (Neg P[0]))")
   ]
